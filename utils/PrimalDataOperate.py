fa772c12a747b8a4d399c8d09cc4ca3780aef6ef
fuxihao66 <fuxihao66@gmail.com>
2017-07-28 08:33:03 +0800

    data

--- utils/PrimalDataOperate.py
+++ utils/PrimalDataOperate.py
@@ -3,15 +3,21 @@ import codecs
 import json
 import sys
 import nltk
+    
 
-def read_data_as_a_passage(file_to_read, file_to_write):
+def read_data_as_a_passage(file_to_read):
     passage_list     =  []
     answers_list     =  []
     query_list       =  []
-    description_list =  []
+    # description_list =  []
     with open(file_to_read, 'r', encoding='utf8') as data_file:
         for line in data_file:
             instance = json.loads(line)
+
+            #some answers are blank
+            if instance['answers'] == []:
+                continue
+
             passage = ''
             for sentence in instance['passages']:
                 passage = passage + sentence['passage_text']
@@ -20,29 +26,33 @@ def read_data_as_a_passage(file_to_read, file_to_write):
             answer = ''
             for answer_str in instance['answers']:
                 answer = answer + answer_str
-            
             answers_list.append(answer) 
-            #some answers are blank???????????????
-
+    
             query_list.append(instance['query'])
-            description_list.append(instance['query_type'])
+            # description_list.append(instance['query_type'])
 
     data_to_write = {}
-    # data_to_write['passages']     =  passage_list
-    # data_to_write['answers']      =  answers_list
-    # data_to_write['queries']      =  query_list
+    data_to_write['passages']     =  passage_list
+    data_to_write['answers']      =  answers_list
+    data_to_write['queries']      =  query_list
     # data_to_write['descriptions'] =  description_list
     
+    return data_to_write
 
-
-    # ### this method cannot encode some character very well
+    ### this method cannot encode some character very well
     # with open(file_to_write, 'w') as modified_data_file:
     #     modified_data_file.write(json.dumps(data_to_write)) 
+
+
+
+
+
 
 def Tokenize(sentence):
     return [token.replace("''", '"').replace("``", '"') for token in nltk.word_tokenize(sentence)]
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print('parameters error')
     else:
-        read_data_as_a_passage(sys.argv[1], sys.argv[2])
+        dict_of_sentences = read_data_as_a_passage(sys.argv[1])
+        print(Tokenize((dict_of_sentences['queries'])[0]))
--- utils/my_data_utils.py
+++ utils/my_data_utils.py
@@ -1,7 +1,27 @@
-class DataSet:
-    def __init__(self, data):
-        self.data = data
-        self.total_num = self.get_data_size()
+# class DataSet:
+#     def __init__(self, data):
+#         self.data = data
+#         self.total_num = self.get_data_size()
 
-    def get_data_size(self):
-        
+#     def get_data_size(self):
+
+'''
+1. how to deal with answers, passages and queries
+2. what kind of data structure
+'''
+
+def read_from_file(file_type):
+    if file_type == 'train':
+
+    elif file_type == 'dev':
+    
+    return dict_of_data
+def read_data_from_dict(batch_size, data):
+    exampels_num = len(data)
+    batch_num = exampels_num/batch_size
+    data_set = []
+    for _ in range(batch_num):
+
+    return data_set
+def get_batch(data_set, idx):
+    return data_set[idx]
