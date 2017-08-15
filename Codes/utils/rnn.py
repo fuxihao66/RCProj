df75b96a79d33e093fd756ffa759a018fd6b8a1f
fuxihao66 <fuxihao66@gmail.com>
2017-08-15 14:38:43 +0800

    all

--- Codes/utils/rnn.py
+++ Codes/utils/rnn.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from tensorflow.python.ops.rnn import dynamic_rnn as _dynamic_rnn, \
     bidirectional_dynamic_rnn as _bidirectional_dynamic_rnn
-from tensorflow.python.ops.rnn import bidirectional_rnn as _bidirectional_rnn
+# from tensorflow.python.ops.rnn import bidirectional_rnn as _bidirectional_rnn
 
-from my.tensorflow import flatten, reconstruct
+from utils.general import flatten, reconstruct
 
 
 def dynamic_rnn(cell, inputs, sequence_length=None, initial_state=None,
@@ -64,19 +64,19 @@ def bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs, sequence_length=None,
     return (fw_outputs, bw_outputs), final_state
 
 
-def bidirectional_rnn(cell_fw, cell_bw, inputs,
-                      initial_state_fw=None, initial_state_bw=None,
-                      dtype=None, sequence_length=None, scope=None):
+# def bidirectional_rnn(cell_fw, cell_bw, inputs,
+#                       initial_state_fw=None, initial_state_bw=None,
+#                       dtype=None, sequence_length=None, scope=None):
 
-    flat_inputs = flatten(inputs, 2)  # [-1, J, d]
-    flat_len = None if sequence_length is None else tf.cast(flatten(sequence_length, 0), 'int64')
+#     flat_inputs = flatten(inputs, 2)  # [-1, J, d]
+#     flat_len = None if sequence_length is None else tf.cast(flatten(sequence_length, 0), 'int64')
 
-    (flat_fw_outputs, flat_bw_outputs), final_state = \
-        _bidirectional_rnn(cell_fw, cell_bw, flat_inputs, sequence_length=flat_len,
-                           initial_state_fw=initial_state_fw, initial_state_bw=initial_state_bw,
-                           dtype=dtype, scope=scope)
+#     (flat_fw_outputs, flat_bw_outputs), final_state = \
+#         _bidirectional_rnn(cell_fw, cell_bw, flat_inputs, sequence_length=flat_len,
+#                            initial_state_fw=initial_state_fw, initial_state_bw=initial_state_bw,
+#                            dtype=dtype, scope=scope)
 
-    fw_outputs = reconstruct(flat_fw_outputs, inputs, 2)
-    bw_outputs = reconstruct(flat_bw_outputs, inputs, 2)
-    # FIXME : final state is not reshaped!
-    return (fw_outputs, bw_outputs), final_state
+#     fw_outputs = reconstruct(flat_fw_outputs, inputs, 2)
+#     bw_outputs = reconstruct(flat_bw_outputs, inputs, 2)
+#     # FIXME : final state is not reshaped!
+#     return (fw_outputs, bw_outputs), final_state
