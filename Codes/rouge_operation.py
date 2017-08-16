52bbb4158bf91bb990af5bbada9ad661c48354b2
fuxihao66 <fuxihao66@gmail.com>
2017-08-16 15:30:15 +0800

    all

--- Codes/rouge_operation.py
+++ Codes/rouge_operation.py
@@ -78,6 +78,8 @@ def get_highest_rl_span(para, reference, max_gap):
     index_start, index_stop = get_idx_sublist(word_token_para, substring)
     # print(para[best_span_start: best_span_end])
     print(max_rouge)
+    print(substring)
+    print(word_token_para)
     return trans_idx_1dto2d(index_start, index_stop, sent_token_para), True
 
 def get_selected_span(para, selected_span):
