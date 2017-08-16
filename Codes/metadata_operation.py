8b1fcc21c1104afba11d467d5132f015dfbacdaa
fuxihao66 <fuxihao66@gmail.com>
2017-08-16 22:32:41 +0800

    all

--- Codes/metadata_operation.py
+++ Codes/metadata_operation.py
@@ -92,7 +92,7 @@ def process_tokens(temp_tokens):
     tokens = []
     for token in temp_tokens:
         flag = False
-        l = ("\*","-", "\u2212", "\u2014", "\u2013", "/", "~", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
+        l = ("-", "\u2212", "\u2014", "\u2013", "/", "~", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
         # \u2013 is en-dash. Used for number to nubmer
         # l = ("-", "\u2212", "\u2014", "\u2013")
         # l = ("\u2013",)
