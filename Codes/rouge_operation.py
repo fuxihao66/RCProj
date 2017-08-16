655b07d5f176982ef14f935633ed0c6acfc90a52
fuxihao66 <fuxihao66@gmail.com>
2017-08-16 18:29:23 +0800

    all

--- Codes/rouge_operation.py
+++ Codes/rouge_operation.py
@@ -46,9 +46,8 @@ def trans_idx_1dto2d(idx_start, idx_stop, list2d):
                 start_idxs_2d = [i, j]
             if end_flag == idx_stop:
                 end_idxs_2d = [i, j]
-    print(start_idxs_2d)
-    
-    print(end_idxs_2d)
+    print(idx_start)
+    print(idx_stop)
 
     return [start_idxs_2d, end_idxs_2d]
 def get_highest_rl_span(para, reference, max_gap):
