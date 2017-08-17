3b4afd51c818d695990368d35be57309a4f959ca
fuxihao66 <fuxihao66@gmail.com>
2017-08-17 13:54:05 +0800

    all

--- Codes/metadata_operation.py
+++ Codes/metadata_operation.py
@@ -53,6 +53,7 @@ def read_metadata(file_to_read):
     data_dict['passage_selected'] = selected_passage_list
     # data_dict['descriptions'] =  description_list
     print(data_dict['passages'][3805])
+    print('all finished')
     return data_dict
 
     ### this method cannot encode some character very well
