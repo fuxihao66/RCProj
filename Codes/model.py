9b1e56213d6f02529c0c6d9897b33a6904b69c88
fuxihao66 <fuxihao66@gmail.com>
2017-08-20 17:01:00 +0800

    all

--- Codes/model.py
+++ Codes/model.py
@@ -153,7 +153,7 @@ class Model:
 
 
 
-        cell = BasicLSTMCell(d, state_is_tuple=True)
+        cell = BasicLSTMCell(d, state_is_tuple=True, reuse=False)
         d_cell = SwitchableDropoutWrapper(cell, self.is_train, input_keep_prob=config.input_keep_prob)
         x_len = tf.reduce_sum(tf.cast(self.x_mask, 'int32'), 2)  # [N, M]
         q_len = tf.reduce_sum(tf.cast(self.q_mask, 'int32'), 1)  # [N]
