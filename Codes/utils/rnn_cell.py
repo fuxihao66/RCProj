a44fa08cdb86c02bbab049d09210d8ceb7b32d82
fuxihao66 <fuxihao66@gmail.com>
2017-08-20 20:30:38 +0800

    all

--- Codes/utils/rnn_cell.py
+++ Codes/utils/rnn_cell.py
@@ -10,7 +10,7 @@ class SwitchableDropoutWrapper(DropoutWrapper):
              seed=None):
         super(SwitchableDropoutWrapper, self).__init__(cell, input_keep_prob=input_keep_prob, output_keep_prob=output_keep_prob, seed=seed)
         self.is_train = is_train
-        print(self.weights)
+
     def __call__(self, inputs, state, scope=None):
         outputs_do, new_state_do = super(SwitchableDropoutWrapper, self).__call__(inputs, state, scope=scope)
         tf.get_variable_scope().reuse_variables()
