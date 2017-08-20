903cbeac90f4d396010997c888eb88e5bdabfd9b
fuxihao66 <fuxihao66@gmail.com>
2017-08-20 20:29:01 +0800

    all

--- Codes/utils/rnn_cell.py
+++ Codes/utils/rnn_cell.py
@@ -8,10 +8,9 @@ from utils.nn import linear, softsel, double_linear_logits
 class SwitchableDropoutWrapper(DropoutWrapper):
     def __init__(self, cell, is_train, input_keep_prob=1.0, output_keep_prob=1.0,
              seed=None):
-        super(SwitchableDropoutWrapper, self).__init__(cell, input_keep_prob=input_keep_prob, output_keep_prob=output_keep_prob,
-                                                       seed=seed)
+        super(SwitchableDropoutWrapper, self).__init__(cell, input_keep_prob=input_keep_prob, output_keep_prob=output_keep_prob, seed=seed)
         self.is_train = is_train
-
+        print(self.weights)
     def __call__(self, inputs, state, scope=None):
         outputs_do, new_state_do = super(SwitchableDropoutWrapper, self).__call__(inputs, state, scope=scope)
         tf.get_variable_scope().reuse_variables()
