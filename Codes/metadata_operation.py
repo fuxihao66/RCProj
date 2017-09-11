66c52ce198006acf19098d4f2d209de63c5387d9
fuxihao66 <fuxihao66@gmail.com>
2017-09-11 15:13:13 +0800

    shuffle

--- Codes/metadata_operation.py
+++ Codes/metadata_operation.py
@@ -18,10 +18,10 @@ def read_metadata(file_to_read):
     with open(file_to_read, 'r', encoding='utf8') as data_file:
         for i, line in enumerate(tqdm(data_file)):
 
-            if passage_list == 1000:
+            if len(passage_list) == 1000:
                 break
 
-            
+
             instance = json.loads(line)
 
             #some answers are blank
