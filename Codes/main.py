525e89f12d0ddee1f800229e8859593634c9c246
fuxihao66 <fuxihao66@gmail.com>
2017-09-13 09:53:05 +0800

    shuffle

--- Codes/main.py
+++ Codes/main.py
@@ -15,16 +15,15 @@ from pre_processing import *
 
 def main(config):
     # set_dirs(config)
-    with tf.device(config.device):
-        # if config.mode == 'train':
-        _train(config)
-        
+    # with tf.device(config.device):
+        if config.mode == 'train':
+            _train(config)
         # elif config.mode == 'test':
         #     _test(config)
         # elif config.mode == 'forward':
         #     _forward(config)
-        # else:
-        #     raise ValueError("invalid value for 'mode': {}".format(config.mode))
+        else:
+            raise ValueError("invalid value for 'mode': {}".format(config.mode))
 
 
 def _train(config):
