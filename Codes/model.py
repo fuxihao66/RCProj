c739dad175daa3381507e33c98d26091b1efda8f
fuxihao66 <fuxihao66@gmail.com>
2017-09-14 14:56:26 +0800

    lr

--- Codes/model.py
+++ Codes/model.py
@@ -292,6 +292,7 @@ class Model:
             else:
                 [j, k] = [0, 0]
                 [j2, k2] = [0,0]
+                print(i)
             y[i, j, k] = True
             y2[i, j2, k2] = True
 
