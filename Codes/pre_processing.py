07e14456fb83e436c1fa8dc8960357aa1189baab
fuxihao66 <fuxihao66@gmail.com>
2017-09-21 16:11:18 +0800

    ans

--- Codes/pre_processing.py
+++ Codes/pre_processing.py
@@ -173,13 +173,13 @@ class DataSet:
 
 if __name__ == '__main__':
 
-    # dev_ans = []
-    # for i in range(24):
-    #     with open('''/home/zhangs/RC/data/ans_dev{}.json'''.format(i), 'r') as ans:
-    #         for line in tqdm(ans):
-    #             instance = json.loads(line)
-    #             dev_ans.extend(instance)
-    # write_to_file('''/home/zhangs/RC/data/dev_answers.json''', dev_ans)
+    dev_ans = []
+    for i in range(25):
+        with open('''/home/zhangs/RC/data/ans_train{}.json'''.format(i), 'r') as ans:
+            for line in tqdm(ans):
+                instance = json.loads(line)
+                dev_ans.extend(instance)
+    write_to_file('''/home/zhangs/RC/data/train_answers.json''', dev_ans)
     # print(len(dev_ans))
     # de = []
     # with open('''/home/zhangs/RC/data/train_answers.json''', 'r') as fi:
@@ -189,14 +189,14 @@ if __name__ == '__main__':
     #                 de.append(ins)
     # print(len(de))
              
-    train_data_dict = read_metadata('''/home/zhangs/RC/data/train_v1.1.json''')
+    # train_data_dict = read_metadata('''/home/zhangs/RC/data/train_v1.1.json''')
     # dev_data_dict   = read_metadata('''/home/zhangs/RC/data/dev_v1.1.json''')
 
-    train_data = DataSet(train_data_dict)
+    # # train_data = DataSet(train_data_dict)
     # dev_data   = DataSet(dev_data_dict)
-    # print('start operating answers')
-    train_data.operate_answers(25)
-    print('operating answers successfully')
+    # # print('start operating answers')
+    # dev_data.operate_answers(20)
+    # print('operating answers successfully')
 
 
 
