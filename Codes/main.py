edd6e6b3511dd8166cfcb4636dc727d3057ade6b
fuxihao66 <fuxihao66@gmail.com>
2017-09-21 22:13:07 +0800

    dev

--- Codes/main.py
+++ Codes/main.py
@@ -116,9 +116,9 @@ def _train(config):
             print(yp2[i])
             print(dev_data_dict_backup['passages'][j*config.batch_size+i])
             print(dev_data_dict['passages'][j*config.batch_size+i])
-            wordss = batch['x'][i][yp[i][0]:yp2[i][0]+1]
-            wordss[0] = wordss[0][yp[i][1]:]
-            wordss[len(wordss)-1] = wordss[len(wordss)-1][:yp2[i][1]+1]
+            wordss = batch['x'][i]
+            # wordss[0] = wordss[0][yp[i][1]:]
+            # wordss[len(wordss)-1] = wordss[len(wordss)-1][:yp2[i][1]+1]
             print(wordss)
 
             summary = get_phrase(dev_data_dict_backup['passages'][j*config.batch_size+i], wordss, [yp[i], yp2[i]])
